026a5659a7ad1cae3b6872083f31e6e9ede3cf62
Psychodramallama <simonevdlinden91@gmail.com>
2026-07-03 12:30:26 -0600

    Install.sh

---
+++ Install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "=================================="
+echo "      LlamaStream Installer"
+echo "=================================="
+
+echo ""
+echo "Welcome to LlamaStream!"
